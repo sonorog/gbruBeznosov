4d7ad6b5959a02032a25e6c2535490cb55c77993
sonorog <nosorogof@gmail.com>
2022-03-01 21:47:13 +0300

    additional last comment

---
+++ src/main/java/homeWork2/homework2.java
@@ -0,0 +1,14 @@
+package homeWork2;
+
+public class homework2 {
+    public static void main(String args[] ) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+        }
+        System.out.println("end");
+
+
+
+        }
+
+}
