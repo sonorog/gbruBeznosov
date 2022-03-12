56f0301ce1226eabe23891f323cd516da1642ebb
sonorog <nosorogof@gmail.com>
2022-03-12 17:56:48 +0300

    additional last comment

--- src/main/java/homeWork2/homework2.java
+++ src/main/java/homeWork2/homework2.java
@@ -1,14 +1,20 @@
 package homeWork2;
 
 public class homework2 {
-    public static void main(String args[] ) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("i = " + i);
-        }
-        System.out.println("end");
+    public static void main(String args[]) {
 
+       System.out.println(within10and20(2,3));
 
+    }
 
-        }
+    public static boolean within10and20(int x1, int x2) {
+        int summ = x1 + x2;
 
+        return ((summ >10)&(summ <=20));
+
+    }
 }
+
+
+
+
